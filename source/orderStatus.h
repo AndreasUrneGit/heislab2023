020d08fcb1d3584f0833d73097882b8e4a3a4633
Andreas Urne <urneandreas@gmail.com>
2023-02-22 14:37:15 +0100

    Update orderStatus.h

--- source/orderStatus.h
+++ source/orderStatus.h
@@ -1,4 +1,16 @@
 #pragma once
 #include "driver/elevio.h"
+#include <vector>
 
-N_FLOORS*N_BUTTONS 
+elevMatrix[N_FLOORS][N_BUTTONS];
+
+
+updateMatrix(){
+    for(int f = 0; f < N_FLOORS; f++){
+        for(int b = 0; b < N_BUTTONS; b++){
+            int btnPressed = elevio_callButton(f, b);
+            elevio_buttonLamp(f, b, btnPressed);
+            elevMatrix[f][b] = btnPressed;
+        }
+    }
+}
